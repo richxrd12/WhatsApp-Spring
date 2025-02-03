afa97d71acbe77f38518fb3eb98d45ff8a60b03a
richxrd7 <richardquintanapadron@gmail.com>
2025-02-03 21:57:26 +0000

    Estilos en contactos

---
+++ src/main/java/com/whatsapp/whatsappweb/controllers/ProfileController.java
@@ -0,0 +1,37 @@
+package com.whatsapp.whatsappweb.controllers;
+
+import com.whatsapp.whatsappweb.entities.Usuario;
+import com.whatsapp.whatsappweb.services.UsuarioService;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class ProfileController {
+    private final UsuarioService usuarioService;
+
+    public ProfileController(UsuarioService usuarioService) {
+        this.usuarioService = usuarioService;
+    }
+
+    @GetMapping("/profile")
+    public String profile(
+            HttpSession session,
+            Model model
+    ) {
+        Usuario usuario = (Usuario) session.getAttribute("usuario");
+
+        model.addAttribute("usuario", usuario);
+
+        return "profile";
+    }
+
+    @PostMapping("/profile")
+    public String updateProfile(){
+
+        return "redirect:/profile";
+    }
+}
