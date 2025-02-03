8111e2f927b0c86b7734d714cf06bc71899c9082
richxrd7 <richardquintanapadron@gmail.com>
2025-02-03 16:46:11 +0000

    Creada la clase Mensaje para gestionar los mensajes

--- src/main/java/com/whatsapp/whatsappweb/entities/Mensaje.java
+++ src/main/java/com/whatsapp/whatsappweb/entities/Mensaje.java
@@ -3,8 +3,13 @@ package com.whatsapp.whatsappweb.entities;
 import lombok.Data;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.time.LocalDateTime;
+
 @Data
 @Document(collection = "mensajes")
 public class Mensaje {
-
+    private String usuarioEmisor;
+    private String usuarioReceptor;
+    private LocalDateTime fecha;
+    private String texto;
 }
